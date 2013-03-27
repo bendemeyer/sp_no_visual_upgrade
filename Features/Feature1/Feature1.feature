c0759e508aae008d7219d65a1f89512d16ec21b9
bdemeyer <ben@bendemeyer.com>
2013-03-27 16:23:34 -0500

    Initial commit

---
+++ Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="a26eaff2-93e1-4ee0-84b3-6bff42bfdefe" description="Turning on this feature will cause new subsites created througout this site collection to use SharePoint UI version 3 (the default UI for SharePoint 2007). This should be turned on for all sites migrated from SharePoint 2007 to 2010 without performing the visual upgrade." featureId="a26eaff2-93e1-4ee0-84b3-6bff42bfdefe" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="Prevent Visual Upgrade on New Subsites" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="5d809b10-ec44-4196-9c00-990c8f5f0b49" />
+  </projectItems>
+</feature>
---
+++ PreventVisualUpgrade/PreventVisualUpgrade.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Security;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+
+namespace BeenJammin.SharePoint.PreventVisualUpgrade
+{
+    /// <summary>
+    /// Web Events
+    /// </summary>
+    public class PreventVisualUpgrade : SPWebEventReceiver
+    {
+       /// <summary>
+       /// A site was provisioned.
+       /// </summary>
+       public override void WebProvisioned(SPWebEventProperties properties)
+       {
+           base.WebProvisioned(properties);
+           using (SPWeb oWeb = properties.Web)
+           {
+               string parentMaster = oWeb.ParentWeb.MasterUrl;
+               string parentCustom = oWeb.ParentWeb.CustomMasterUrl;
+               oWeb.Site.AllowUnsafeUpdates = true;
+               oWeb.AllowUnsafeUpdates = true;
+               oWeb.UIVersionConfigurationEnabled = true;
+               oWeb.UIVersion = 3;
+               oWeb.CustomMasterUrl = parentCustom;
+               oWeb.MasterUrl = parentMaster;
+               oWeb.AllProperties["__InheritsCustomMasterUrl"] = "True";
+               oWeb.AllProperties["__InheritsMasterUrl"] = "True";
+               oWeb.Update();
+               oWeb.AllowUnsafeUpdates = false;
+               oWeb.Site.AllowUnsafeUpdates = false;
+           }
+       }
+
+
+    }
+}
